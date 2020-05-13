3412c2bcfdd5f40056041133b1057f3bdfd61006
jessie-computes <60405600+jessie-computes@users.noreply.github.com>
2020-05-13 10:38:11 -0400

    O auth create (#24)
    * Redirected to spotify login
    
    * created the login oAuth for spotify

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Champions from "./containers/Champions";
 import Search from "./containers/Search";
+import LoginButton from "./components/LoginButton/loginButton";
+import LoggedIn from "./components/LoggedIn/LoggedIn";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Route exact path="/" component={Champions} />
       <Route exact path="/champions" component={Champions} />
       <Route exact path="/champions/:id" component={Search} />
+      <Route exact path = "/" component={LoginButton}/>
+      <Route exact path="/spotify" render={routeProps => (<LoggedIn {...routeProps}/>)}/>
     </Router>
   );
 }
