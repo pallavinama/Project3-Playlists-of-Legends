177c0be115dc294be46650cf9cd59bc8610d4a17
JohnS <senakj2011@hotmail.com>
2020-05-14 17:39:26 -0400

    committing to review changes

--- client/src/components/SearchBar/FilterSearch.jsx
+++ client/src/components/SearchBar/FilterSearch.jsx
@@ -1,7 +1,35 @@
 // import React, { Component } from 'react';
 
-// class FilterSearch extends Component {
-//     filteredChampions = champions.filter((champion) => {
+// // class FilterSearch extends Component {
+//     class FilterSearch extends React.Component {
+//         constructor(){
+//           super();
+//           this.state ={
+//             users: ['abc',"pdsa","eccs","koi"],
+//             input: '',
+//           }
+//         }
+      
+//         onChangeHandler(e){
+//           this.setState({
+//             input: e.target.value,
+//           })
+//         }
+      
+//         render (){
+//             const list = this.state.users
+//               .filter(d => this.state.input === '' || d.includes(this.state.input))
+//               .map((d, index) => <li key={index}>{d}</li>);
+      
+//           return (<div>
+//             <input value={this.state.input} type="text" onChange={this.onChangeHandler.bind(this)}/>
+//               <ul>{list}</ul>
+//             </div>)
+//         }
+//       }
+      
+//       ReactDOM.render(<First/>,document.getElementById('root'));
+    //     filteredChampions = champions.filter((champion) => {
 //         const regex = new RegExp(this.state.searchTerm, "gi");
 //         return champion.name.first.match(regex);
 //       });
--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,8 +1,27 @@
 import React from "react";
+import React, { Component } from 'react';
 import "./SearchBar.css";
 import "./FilterSearch";
 
-const SearchBar = () => {
+class FilterSearch extends React.Component {
+    constructor(){
+      super();
+      this.state ={
+        users: ['abc',"pdsa","eccs","koi"],
+        input: '',
+      }
+    }
+  
+    onChangeHandler(e){
+      this.setState({
+        input: e.target.value,
+      })
+    }
+  
+    render (){
+        const list = this.state.users
+          .filter(d => this.state.input === '' || d.includes(this.state.input))
+          .map((d, index) => <li key={index}>{d}</li>);
   return (
     <div>
       <form className="form-inline my-2 my-lg-0">
@@ -11,6 +30,8 @@ const SearchBar = () => {
           type="search"
           placeholder="Search"
           aria-label="Search"
+          value={this.state.input}
+          onChange={this.onChangeHandler.bind(this)}
         ></input>
         <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
           Search
@@ -18,6 +39,7 @@ const SearchBar = () => {
       </form>
     </div>
   );
+};
 };
 
 export default SearchBar;
