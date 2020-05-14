434ca5fbdecbd5a402801d4564c607dc5c95f214
Snookie <kieransnook35@gmail.com>
2020-05-14 17:25:40 -0400

    made a jumbotron for a welcome message for when you are on champions page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-
+import WelcomeCard from "./components/Jumbotron/Jumbotron"
 
 import LoggedIn from "./components/LoggedIn/LoggedIn";
 import Champions from "./containers/Champions";
@@ -12,7 +12,10 @@ function App() {
   return (
     <Router>
       <Navbar />
-      
+      <WelcomeCard />
+      <br/>
+      <br/>
+      <br/>
       <Route exact path="/champions" component={Champions} />
       <Route exact path="/champions/:id" component={Search} />
       <Route exact path="/spotify" render={routeProps => (<LoggedIn {...routeProps}/>)}/>
