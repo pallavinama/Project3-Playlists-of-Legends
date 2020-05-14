490e67fc0ba55f2b75c2901b0d1ed2b8916e4973
kieransnook <kieransnook35@gmail.com>
2020-05-14 17:51:37 -0400

    Images (#35)
    * finished list and edited
    
    * new revisions
    
    * champions cards added to images
    
    * got login button to work in nav bar
    
    * cleaned up nav
    
    * edited cards to display differently
    
    * made a jumbotron for a welcome message for when you are on champions page
    
    Co-authored-by: JohnS <senakj2011@hotmail.com>

---
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+
+
+const WelcomeCard = () => {
+    return (
+        <div className="Jumbotron">
+  
+    <h1>WELCOME SUMMONERS</h1>
+    <p>Select the champion you are currently playing to get a playlist that will get you in the right headspace for that champion!</p>
+  </div>
+
+    )
+}
+
+export default WelcomeCard
