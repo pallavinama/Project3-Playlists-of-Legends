a60327dcb9b518a95cb39aaefad107e93144f22d
Alan Ashworth <awashworth927@gmail.com>
2020-05-16 10:47:56 -0400

    Working on ProfileCard to retrieve signed in user info.

--- client/src/containers/Profile.jsx
+++ client/src/containers/Profile.jsx
@@ -1,11 +1,37 @@
 import React, { Component } from "react";
 import ProfileCard from "../components/Card/Profile";
+import { Link } from "react-router-dom";
+import UserDbAPI from "../utils/UserDbAPI";
 
 class Profile extends Component {
+  // state = {
+  //   user: {},
+  // };
+  // constructor(props) {
+  //   super(props);
+  //   console.log(this.props);
+  // }
+
+  componentDidMount() {
+    UserDbAPI.getUser().then((data) => {
+      console.log("web api response " + data);
+      this.setState({ user: data });
+      console.log("user.username " + this.state.user.username);
+    });
+  }
+
   render() {
     return (
       <>
-        <ProfileCard />
+        {/* <Link to={"/user/" + user.key}>
+          <ProfileCard
+            username={user.username}
+            password={user.password}
+            name={user.name}
+            age={user.age}
+            gender={user.gender}
+          />
+        </Link> */}
       </>
     );
   }
