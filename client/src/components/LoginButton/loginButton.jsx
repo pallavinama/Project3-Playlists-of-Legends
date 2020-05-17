39267c4b0b38f9b890848e7b2321a44870c08a03
Jessie Everett <jessieeverett55@gmail.com>
2020-05-17 11:14:11 -0400

    Spotify data pull (#47)
    * Added the scope that allows the application to modify user's playlists
    
    * Added the icons folder, got the icon working on the URL
    
    * Added the links needed for the spotify embed codes for all the playlists associated with each champion
    
    * changed the readme to add all the champion names, genres, and playlist links to code that can be used to insert data into the database
    
    * Created a SpotifyPlaylist.jsx component and edited the LoggedIn componenent
    
    * Created the array of objects for each character id
    
    * Got the right spotify playlist rendering for each champion
    
    * Removed the artist follow button
    
    * All the search bar changes
    
    * Working code
    
    * Edited the import Details statement
    
    * Changed the spotify redirect URI
    
    * removed the search tab from the nav bar and moved the Spotify Login button to the right
    
    * Removed the details button from the nav bar
    
    * Fixed the redirect URI

--- client/src/components/LoginButton/loginButton.jsx
+++ client/src/components/LoginButton/loginButton.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 function loginButton() {
   const my_client_id = "d1c5c07ebd804b5b8667bb33ae85efd7";
   const scopes = "playlist-modify-private";
-  const redirect_uri = "https://rocky-stream-45210.herokuapp.com/";
+  const redirect_uri = "https://rocky-stream-45210.herokuapp.com/spotify";
 
   const link =
     "https://accounts.spotify.com/authorize" +
@@ -18,7 +18,7 @@ function loginButton() {
   const spotifyLogin = () => {
     const my_client_id = process.env.REACT_APP_CLIENT_ID;
     const scopes = "playlist-modify-private";
-    const redirect_uri = "https://rocky-stream-45210.herokuapp.com/";
+    const redirect_uri = "https://rocky-stream-45210.herokuapp.com/spotify";
 
     axios.get(
       "https://accounts.spotify.com/authorize" +
